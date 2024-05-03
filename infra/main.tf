a12def974bc0705b042ae595bbaeec2bd4b5527f
Mohammed Allam <moealllam@gmail.com>
2024-05-03 12:59:19 -0600

    Refactor VPC module to remove unused code and improve readability

--- infra/main.tf
+++ infra/main.tf
@@ -4,21 +4,21 @@ module "vpc" {
     subnet_cidr = var.subnet_cidr
 }
  
-module "sg" {
-    source = "./modules/sg"
-    vpc_id = module.vpc.vpc_id
-}
+# module "sg" {
+#     source = "./modules/sg"
+#     vpc_id = module.vpc.vpc_id
+# }
 
-module "ec2" {
-    source = "./modules/ec2"
-    sg_id = module.sg.sg_id
-    subnets = module.vpc.subnet_ids
-}
+# module "ec2" {
+#     source = "./modules/ec2"
+#     sg_id = module.sg.sg_id
+#     subnets = module.vpc.subnet_ids
+# }
 
-module "elb" {
-    source = "./modules/elb"
-    sg_id = module.sg.sg_id
-    subnets = module.vpc.subnet_ids
-    vpc_id = module.vpc.vpc_id
-    instances = module.ec2.instances
-}
+# module "elb" {
+#     source = "./modules/elb"
+#     sg_id = module.sg.sg_id
+#     subnets = module.vpc.subnet_ids
+#     vpc_id = module.vpc.vpc_id
+#     instances = module.ec2.instances
+# }
